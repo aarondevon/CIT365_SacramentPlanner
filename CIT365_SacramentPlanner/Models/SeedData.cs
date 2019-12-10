e62a332fe9e1c1042acecc38bb3753f8a3d11701
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-12-10 17:24:46 -0600

    Create seed data

--- CIT365_SacramentPlanner/Models/SeedData.cs
+++ CIT365_SacramentPlanner/Models/SeedData.cs
@@ -1,11 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CIT365_SacramentPlanner.Data;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CIT365_SacramentPlanner.Models
 {
-    public class SeedData
+    public static class SeedData
     {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MeetingContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MeetingContext>>()))
+            {
+                // Look for any movies.
+                if (context.Meeting.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Meeting.AddRange(
+                    new Meeting
+                    {
+                        MeetingDate = DateTime.Parse("2019-12-08"),
+                        Conductor = "Jeff Hansen",
+                        OpeningHymnNumber = 201,
+                        SacramentHymnNumber = 100,
+                        RestHymnNumber = 20,
+                        ClosingHymn = 300,
+                        OpeningPrayer = "Frank Heart",
+                        ClosingPrayer = "Katie Larsen",
+                        SpeakerOne = "Mike Jones",
+                        SpeakerTwo = "Ryan Sawyer",
+                        SpeakerThree = "John Armstrong"
+                    },
+
+                    new Meeting
+                    {
+                        MeetingDate = DateTime.Parse("2019-11-03"),
+                        Conductor = "Mike Barns",
+                        OpeningHymnNumber = 89,
+                        SacramentHymnNumber = 82,
+                        RestHymnNumber = 102,
+                        ClosingHymn = 92,
+                        OpeningPrayer = "Fred Hinzer",
+                        ClosingPrayer = "Zack Manchester"
+                    },
+
+                    new Meeting
+                    {
+                        MeetingDate = DateTime.Parse("2019-11-10"),
+                        Conductor = "Jeff Hansen",
+                        OpeningHymnNumber = 203,
+                        SacramentHymnNumber = 93,
+                        RestHymnNumber = 83,
+                        ClosingHymn = 73,
+                        OpeningPrayer = "Kevin Heart",
+                        ClosingPrayer = "Bryce Willson",
+                        SpeakerOne = "Magnum Jenkins",
+                        SpeakerTwo = "Anthony Stark",
+                        SpeakerThree = "Bruce Wayne"
+                    },
+
+                    new Meeting
+                    {
+                        MeetingDate = DateTime.Parse("2019-11-17"),
+                        Conductor = "Mike Barns",
+                        OpeningHymnNumber = 304,
+                        SacramentHymnNumber = 204,
+                        RestHymnNumber = 104,
+                        ClosingHymn = 94,
+                        OpeningPrayer = "Steve Rogers",
+                        ClosingPrayer = "Katie Larsen",
+                        SpeakerOne = "Diana Prince ",
+                        SpeakerTwo = "Natasha Romanoff",
+                        SpeakerThree = "Bill Jensen"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
     }
 }
