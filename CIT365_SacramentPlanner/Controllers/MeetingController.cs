d4bc05fc8811dd14255702f36206ab46fdc82adc
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-12-10 14:59:20 -0600

    Add controller and create initial model

---
+++ CIT365_SacramentPlanner/Controllers/MeetingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace CIT365_SacramentPlanner.Controllers
+{
+    public class MeetingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CIT365_SacramentPlanner/Models/Meeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CIT365_SacramentPlanner.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime MeetingDate { get; set; }
+        public string Conductor { get; set; }
+        public string OpeningHymn { get; set; }
+        public string SacramentHymn { get; set; }
+        public string RestHymn { get; set; }
+        public string ClosingHymn { get; set; }
+        public string OpeningPrayer { get; set; }
+        public string ClosingPrayer { get; set; }
+        public string SpeakerOne { get; set; }
+        public string SpeakerTwo { get; set; }
+        public string SpeakerThree { get; set; }
+
+    }
+}
