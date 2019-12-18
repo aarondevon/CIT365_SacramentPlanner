92e27768280a482daf31f5d8cc5755ae33a2f075
RHunt20 <22162274+RHunt20@users.noreply.github.com>
2019-12-18 15:50:22 -0600

    Table color grey and fixed rest hymn range

--- CIT365_SacramentPlanner/Models/Meeting.cs
+++ CIT365_SacramentPlanner/Models/Meeting.cs
@@ -26,7 +26,7 @@ namespace CIT365_SacramentPlanner.Models
         [Required]
         public int SacramentHymnNumber { get; set; }
         [Display(Name = "Rest Hymn")]
-        [RegularExpression(@"[1-341]")]
+        [Range(1, 341)]
         public int? RestHymnNumber { get; set; }
         [Display(Name = "Closing Hymn")]
         [Range(1, 341)]
